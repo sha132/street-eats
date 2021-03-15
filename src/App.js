dd932f103551234336e90031bf82ff45db213134
sha132 <snicholas1252@fdnyhsbk.org>
2021-03-15 16:49:47 +0000

    done

--- src/App.js
+++ src/App.js
@@ -2,18 +2,27 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Splash from './components/Splash';
 import Testimonials from './components/Testimonials';
-// import ReviewCard from './components/ReviewCard';
-// import reviews from './review_data';
+ import ReviewCard from './components/ReviewCard';
+ import reviews from './review_data';
 import './App.css';
 
 function App() {
+     let testimonial = "Street Eats is the best food review site that has ever been built. Now I can pick the best food cart with confidence instead of wondering if I'm getting a raw deal."
   return(
     <div className="App">
       <Navbar/>
       <Splash/>
-      <Testimonials/>
+      <Testimonials userTestimonial={testimonial} />
       <div className = "container">
         <div className="row">
+             <ReviewCard
+            name={ reviews[0].name }
+            headline={ reviews[0].headline }
+            summary={ reviews[0].summary }
+            stars={ reviews[0].stars }
+            posted={ reviews[0].posted }
+             />
+
 
         </div>
       </div>
--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -7,7 +7,7 @@ function Testimonial(props) {
       <div className="row">
         <div className="col-6 offset-3 testimonial">
           <h3 className="secondary-text-testimonial">Testimonials</h3>
-          <p className="paragraph-text"></p>
+          <p className="paragraph-text">{props.userTestimonial}</p>
         </div>
       </div>
     </div>
